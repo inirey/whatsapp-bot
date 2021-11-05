78f2b19f5f5b079a3d963925f598e0e2841213b9
Revita <gemr412@gmail.com>
2021-11-05 16:17:54 +0700

    Update version Bailys

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ nocache("./rey.js", (module) => console.log(`${module} is now updated!`));
 
 const starts = async (client = new WAConnection()) => {
   client.logger.level = "warn";
-  client.version = [2, 2123, 8];
+  client.version = [2, 2143, 3];
   client.browserDescription = ["Reyganz", "safari", "3.0"];
   console.log(banner.string);
   client.on("qr", () => {
