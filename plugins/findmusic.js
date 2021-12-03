a07e14cbad2517d9415e4c06cacb809f717d5b1e
Reyレイ・セカ <gansrey97@gmail.com>
2021-12-03 11:59:44 +0700

    Add files via upload

---
+++ plugins/findmusic.js
@@ -0,0 +1,34 @@
+//Created By Rey
+
+let FormData = require('form-data')
+let axios = require('axios')
+
+let handler = async (m, { conn, usedPrefix, command }) => {
+  let q = m.quoted ? m.quoted : m
+  let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
+  if (!/video|audio/.test(mime)) throw `Reply to the music you want to find with a caption\n\n_Reply music atau audio dengan caption_ *${usedPrefix + command}*`
+ m.reply('Wait,Looking For....')
+				const bodyForm = new FormData()
+			        bodyForm.append('audio', await q.download(), 'music.mp3')
+           			bodyForm.append('apikey', 'administrator')
+           			axios('https://api.zeks.me/api/searchmusic?apikey=administrator&audio=https://media1.vocaroo.com/mp3/1ePZvzWDKxIh', {
+                		method: 'POST',
+                		headers: {
+				"Content-Type": "multipart/form-data",
+        			...bodyForm.getHeaders()
+                		},
+                		data: bodyForm
+            			})
+                		.then(({data}) =>{
+				  m.reply(`*Song Found!*\n\n*Title* : ${data.data.title}\n*Artist* : ${data.data.artists}\n*Genre* : ${data.data.genre}\n*Album* : ${data.data.album}\n*Release* : ${data.data.release_date}`)
+				}).catch(() => {
+				m.reply('Song Not Found!')
+				})
+				
+}
+handler.help = ['whatmusic']
+handler.tags = ['tools']
+
+handler.command = /^(whatmusic|find)$/i
+
+module.exports = handler
---
+++ plugins/menu.js
@@ -0,0 +1,223 @@
+let levelling = require('../lib/levelling')
+let { MessageType } = require('@adiwajshing/baileys')
+let fs = require('fs')
+let path = require('path')
+let fetch = require('node-fetch')
+let moment = require('moment-timezone')
+let tags = {
+    'game': 'G A M E  M E N U',
+    'xp': 'E X P  M E N U',
+    'nsfw': 'N S F W  M E N U',
+    'random': 'R A N D O M  I M A G E',
+    'sticker': 'S T I C K E R',
+    'wallhp': 'W A L L P A P E R',
+    'kerang': 'K E R A N G  M E N U',
+    'quotes': 'T E X T  M E N U',
+    'group': 'G R O U P  M E N U',
+    'internet': 'S E A R C H I N G',
+    'nulis': 'M A K E R  M E N U',
+    'downloader': 'D O W N L O A D',
+    'tools': 'T O O L S  M E N U',
+    'fun': 'F U N  M E N U',
+    'database': 'D A T A B A S E',
+    'vote': 'V O T E  M E N U',
+    'absen': 'A B S E N S I',
+    'quran': 'I S L A M I C',
+    'audio': 'V O I C E  M E N U',
+    'info': 'I N F O  M E N U',
+}
+
+const defaultMenu = {
+  before: `
+
+Hai, %name! 👋 Welcome have a nice day ✨
+
+    *I N F O  U S E R S*
+
+• *NAME:* %name
+• *LIMIT:* %limit
+• *ROLE:* %role
+• *LEVEL:* %level %exp / %maxexp
+• *XP:* %totalexp
+
+
+    *E R Z A  I N F O*
+
+• *DATE:* %week %date
+• *DATE ISLAMI*: %dateIslamic
+• *TIME:* %time
+• *RUNTIME:* %uptime
+• *USERS:* %rtotalreg
+• *WEB:* https://erza-info.ml
+
+
+    *O W N E R  I N F O*
+
+• *OWNER:* Rey Sekha
+• *GITHUB:* https://github.com/inirey
+• *WEB:* https://reysekha-web.eu.org
+• *INSTAGRAM:* https://instagram.com/_sekhaa
+
+_-_-_-_-_-_-_-_-_-_-_-_-_-_
+
+%readmore`.trimStart(),
+  header: '*%category*\n\n',
+  body: '• %cmd',
+  footer: '\n',
+  after: `
+*%npmname*
+${'```%npmdesc```'}
+`,
+}
+let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
+  try {
+    let package = JSON.parse(await fs.promises.readFile(path.join(__dirname, '../package.json')).catch(_ => '{}'))
+    let { exp, limit, level, role, registered } = global.db.data.users[m.sender]
+    let { min, xp, max } = levelling.xpRange(level, global.multiplier)
+    let name = registered ? global.db.data.users[m.sender].name : conn.getName(m.sender)
+    let d = new Date(new Date + 3600000)
+    let locale = 'id'
+    // d.getTimeZoneOffset()
+    // Offset -420 is 18.00
+    // Offset    0 is  0.00
+    // Offset  420 is  7.00
+    let weton = ['Pahing', 'Pon', 'Wage', 'Kliwon', 'Legi'][Math.floor(d / 84600000) % 5]
+    let week = d.toLocaleDateString(locale, { weekday: 'long' })
+    let date = d.toLocaleDateString(locale, {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric'
+    })
+    let dateIslamic = Intl.DateTimeFormat(locale + '-TN-u-ca-islamic', {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric'
+    }).format(d)
+    let time = d.toLocaleTimeString(locale, {
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric'
+    })
+    let _uptime = process.uptime() * 1000
+    let _muptime
+    if (process.send) {
+      process.send('uptime')
+      _muptime = await new Promise(resolve => {
+        process.once('message', resolve)
+        setTimeout(resolve, 1000)
+      }) * 1000
+    }
+    let muptime = clockString(_muptime)
+    let uptime = clockString(_uptime)
+    let totalreg = Object.keys(global.db.data.users).length
+    let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
+    let help = Object.values(global.plugins).filter(plugin => !plugin.disabled).map(plugin => {
+      return {
+        help: Array.isArray(plugin.help) ? plugin.help : [plugin.help],
+        tags: Array.isArray(plugin.tags) ? plugin.tags : [plugin.tags],
+        prefix: 'customPrefix' in plugin,
+        limit: plugin.limit,
+        premium: plugin.premium,
+        enabled: !plugin.disabled,
+      }
+    })
+    let groups = {}
+    for (let tag in tags) {
+      groups[tag] = []
+      for (let plugin of help)
+        if (plugin.tags && plugin.tags.includes(tag))
+          if (plugin.help) groups[tag].push(plugin)
+      // for (let tag of plugin.tags)
+      //   if (!(tag in tags)) tags[tag] = tag
+    }
+    conn.menu = conn.menu ? conn.menu : {}
+    let before = conn.menu.before || defaultMenu.before
+    let header = conn.menu.header || defaultMenu.header
+    let body = conn.menu.body || defaultMenu.body
+    let footer = conn.menu.footer || defaultMenu.footer
+    let after = conn.menu.after || (conn.user.jid == global.conn.user.jid ? '' : `Powered by https://wa.me/${global.conn.user.jid.split`@`[0]}`) + defaultMenu.after
+    let _text = [
+      before,
+      ...Object.keys(tags).map(tag => {
+        return header.replace(/%category/g, tags[tag]) + '\n' + [
+          ...help.filter(menu => menu.tags && menu.tags.includes(tag) && menu.help).map(menu => {
+            return menu.help.map(help => {
+              return body.replace(/%cmd/g, menu.prefix ? help : '%p' + help)
+                .replace(/%islimit/g, menu.limit ? '(Limit)' : '')
+                .replace(/%isPremium/g, menu.premium ? '(Premium)' : '')
+                .trim()
+            }).join('\n')
+          }),
+          footer
+        ].join('\n')
+      }),
+      after
+    ].join('\n')
+    text = typeof conn.menu == 'string' ? conn.menu : typeof conn.menu == 'object' ? _text : ''
+    let replace = {
+      '%': '%',
+      p: _p, uptime, muptime,
+      me: conn.user.name,
+      npmname: package.name,
+      npmdesc: package.description,
+      version: package.version,
+      exp: exp - min,
+      maxexp: xp,
+      totalexp: exp,
+      xp4levelup: max - exp <= 0 ? `Siap untuk *${_p}levelup*` : `${max - exp} XP lagi untuk levelup`,
+      github: package.homepage ? package.homepage.url || package.homepage : '[unknown github url]',
+      level, limit, name, weton, week, date, dateIslamic, time, totalreg, rtotalreg, role,
+      readmore: readMore
+    }
+    text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
+    await conn.send2ButtonLoc(m.chat, await (await fetch(fla + 'welcome')).buffer(), text.trim(), 'Customize By Rey ❤️', 'Owner', `${_p}owner`, 'Donasi', `${_p}donasi`, m)
+    //await conn.sendButtonLoc(m.chat, text,trim(), await (await fetch(fla + 'menu')).buffer(),
+    //await conn.send2Button(m.chat, text.trim(), 'made with ❤️ by Sekha', 'Owner', `${_p}owner`, 'Donasi', `${_p}donasi`, m)
+  } catch (e) {
+    conn.reply(m.chat, 'Maaf, menu sedang error', m)
+    throw e
+  }
+}
+handler.help = ['menu', 'help', '?']
+handler.tags = ['main']
+handler.command = /^(menu|help|\?)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.exp = 3
+
+module.exports = handler
+
+const more = String.fromCharCode(1)
+const readMore = more.repeat(1)
+
+function clockString(ms) {
+  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+  return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
+}
+function ucapan() {
+  const time = moment.tz('Asia/Jakarta').format('HH')
+  res = "Selamat dinihari"
+  if (time >= 4) {
+    res = "Selamat pagi"
+  }
+  if (time > 10) {
+    res = "Selamat siang"
+  }
+  if (time >= 15) {
+    res = "Selamat sore"
+  }
+  if (time >= 18) {
+    res = "Selamat malam"
+  }
+  return res
+}
