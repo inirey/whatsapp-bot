3ccb02f1c572bb653678e834242acb9cccbf1171
Sekha <gansrey97@gmail.com>
2022-01-29 16:48:15 +0700

    First

--- config.js
+++ config.js
@@ -51,10 +51,8 @@ global.benar = '✓'
 global.salah = '×'
 global.dikit = 'hampir benar'
 global.footer = '© sekha'
-global.reyganz= 'https://api-reysekha.herokuapp.com/api/wallpaper/justina?apikey='
+global.reyganz= 'https://api.reysekha.xyz/api/wallpaper/justina?apikey='
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&fillColor1Color=%23f2aa4c&fillColor2Color=%23f2aa4c&fillColor3Color=%23f2aa4c&fillColor4Color=%23f2aa4c&fillColor5Color=%23f2aa4c&fillColor6Color=%23f2aa4c&fillColor7Color=%23f2aa4c&fillColor8Color=%23f2aa4c&fillColor9Color=%23f2aa4c&fillColor10Color=%23f2aa4c&fillOutlineColor=%23f2aa4c&fillOutline2Color=%23f2aa4c&backgroundColor=%23101820&text='
-//global.CanvasAPI = '';
-//global.fla = 'https://api-reysekha.herokuapp.com/api/wallpaper/justina?apikey='
 
 global.multiplier = 150 // Semakin tinggi, semakin sulit naik level
 
