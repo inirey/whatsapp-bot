a93aace4c1f0d40406e4b42ccf03affc5d9e22f6
Erz <sekharey@gmail.com>
2022-02-06 02:01:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,11 @@
 let fs = require('fs')
 let chalk = require('chalk')
 
-global.owner = ['6281284760551', '6281284760551'] // Letakan nomor kamu disini
+global.owner = ['6281284760551', '6281284760551'] // Your Number
 
 global.APIs = { // API Prefix
   // nama: 'https://website'
   rey: 'https://api.reysekha.xyz',
-  jawasoft: 'https://api.jawasoft.xyz',
   hardianto: 'https://hardianto.xyz',
   jonaz: 'https://jonaz-api-v2.herokuapp.com',
   neoxr: 'https://api.neoxr.eu.org',
@@ -15,17 +14,14 @@ global.APIs = { // API Prefix
   xteam: 'https://api.xteam.xyz',
   fxc7: 'https://api-xcoders.xyz',
   waifupics: 'https://api.waifu.pics',
-  sekha: 'https://api.sekha.tech',
   cakra: 'https://cakrayp.herokuapp.com',
   zahir: 'https://zahirr-web.herokuapp.com',
   zekais: 'http://zekais-api.herokuapp.com',
   zeks: 'https://api.zeks.me',
 }
-global.APIKeys = { // APIKey nya disini
+global.APIKeys = { // APIKey in here
   // 'https://website': 'apikey'
   'https://api-xcoders.xyz': 'APIKEY',
-  'htttps://api.jawasoft.xyz': 'APIKEY',
-  'https://api.sekha.tech': 'APIKEY',
   'https://api.reysekha.xyz': 'APIKEY',
   'https://hardianto.xyz': 'APIKEY',
   'https://api.neoxr.eu.org': 'APIKEY',
@@ -48,10 +44,11 @@ global.benar = '✓'
 global.salah = '×'
 global.dikit = 'hampir benar'
 global.footer = '© sekha'
+global.lw = 'https://api.reysekha.xyz/api/textpro/greenhoror?apikey=apirey&text='
 global.reyganz= 'https://api.reysekha.xyz/api/wallpaper/justina?apikey='
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&fillColor1Color=%23f2aa4c&fillColor2Color=%23f2aa4c&fillColor3Color=%23f2aa4c&fillColor4Color=%23f2aa4c&fillColor5Color=%23f2aa4c&fillColor6Color=%23f2aa4c&fillColor7Color=%23f2aa4c&fillColor8Color=%23f2aa4c&fillColor9Color=%23f2aa4c&fillColor10Color=%23f2aa4c&fillOutlineColor=%23f2aa4c&fillOutline2Color=%23f2aa4c&backgroundColor=%23101820&text='
 
-global.multiplier = 150 // Semakin tinggi, semakin sulit naik level
+global.multiplier = 9999
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
