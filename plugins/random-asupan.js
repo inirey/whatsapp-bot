4ef30d043c6287a33ba8656f4972660951e334cb
Skh <gansrey97@gmail.com>
2022-02-14 12:43:27 +0700

    Change

--- plugins/random-asupan.js
+++ plugins/random-asupan.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
   await m.reply(wait)
-  let res = `https://api.reysekha.xyz/api/asupan?apikey=APIKEY`
+  let res = `https://api.reysekha.xyz/api/asupan?apikey=apirey`
   await conn.sendFile(m.chat, res, '', `asupan nya gan`, m)
 }
 
