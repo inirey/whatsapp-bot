b2abe900312e20494e77595bd513c91601f87afd
Skh <gansrey97@gmail.com>
2022-02-14 12:58:16 +0700

    Change

--- plugins/cecan.js
+++ plugins/cecan.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
   await m.reply(wait)
-  let res = `https://server-api-rey.herokuapp.com/api/wallpaper/cecan?apikey=apirey`
+  let res = `https://api-reysekha.herokuapp.com/api/wallpaper/cecan?apikey=apirey`
   conn.sendFile(m.chat, res, 'cecan.jpg', `© Reysekha`, m)
 }
 handler.help = ['cecan']
