c78d3166931345f9495a2aa96fe90cffcc51e02c
Skh <gansrey97@gmail.com>
2022-02-14 12:47:56 +0700

    Change

--- plugins/random-blackpink.js
+++ plugins/random-blackpink.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
   await m.reply(wait)
-  let res = `https://server-api-rey.herokuapp.com/api/wallpaper/random/blackpink?apikey=apirey`
+  let res = `https://api-reysekha.herokuapp.com/api/wallpaper/random/blackpink?apikey=apirey`
   conn.sendFile(m.chat, res, 'blackpink.jpg', `© Reysekha`, m)
 }
 handler.help = ['blackpink']
