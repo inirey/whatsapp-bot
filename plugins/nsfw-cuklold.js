86445a0bdb94a448d3d20e726d7d210357845b28
Skh <gansrey97@gmail.com>
2022-02-15 06:51:38 +0700

    Create

--- plugins/nsfw-cuklold.js
+++ plugins/nsfw-cuklold.js
@@ -1,11 +1,12 @@
 let handler = async (m, { conn }) => {
   await m.reply(wait)
-  let res = `https://server-api-rey.herokuapp.com/api/nsfw/cuckold?apikey=apirey`
+  let res = `https://api-reysekha.herokuapp.com/api/nsfw/cuckold?apikey=apirey`
   conn.sendFile(m.chat, res, 'cuklold.jpg', `jangan di buat bahan comli om :v`, m)
 }
 handler.help = ['cuklold']
 handler.tags = ['nsfw']
 handler.command = /^(cuklold)$/i
 handler.limit = true
+handler.nsfw = true
 
 module.exports = handler
