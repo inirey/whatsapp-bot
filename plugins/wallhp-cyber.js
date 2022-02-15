3eef6b737c8d09f3a4022b6c648d65d554ab014e
Skh <gansrey97@gmail.com>
2022-02-15 07:13:24 +0700

    Create

--- plugins/wallhp-cyber.js
+++ plugins/wallhp-cyber.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
   await m.reply(wait)
-  let res = `https://server-api-rey.herokuapp.com/api/wallpaper/cyberspace?apikey=apirey`
+  let res = `https://api.reysekha.xyz/api/wallpaper/cyberspace?apikey=apirey`
   conn.sendFile(m.chat, res, 'cyber.jpg', `© Reysekha`, m)
 }
 handler.help = ['cyberspace']
