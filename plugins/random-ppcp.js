673badea7f44dcb1f865acd964635802fbba82d6
Skh <gansrey97@gmail.com>
2022-02-15 07:22:19 +0700

    Create

---
+++ plugins/random-ppcp.js
@@ -0,0 +1,20 @@
+/*
+Made with By sekha ❤️
+*/
+
+let fetch = require("node-fetch")
+
+let handler = async (m, { conn }) => {
+  let res = await fetch(global.API('rey', '/api/wallpaper/ppcouple', {}, 'apikey'))
+  if (!res.ok) throw await res.text()
+  let json = await res.json()
+  if (!json.status) throw json
+  await conn.sendFile(m.chat, json.result.male, '', 'Man', m)
+  await conn.sendFile(m.chat, json.result.female, '', 'Woman', m)
+}
+handler.help = ['ppcouple', 'ppcp']
+handler.tags = ['random']
+handler.command = /^(pp(cp|couple))$/i
+handler.limit = true
+
+module.exports = handler
