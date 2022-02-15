e8ed131181094f5aacbb0a0c7e5e2fb0f53ae094
Skh <gansrey97@gmail.com>
2022-02-15 08:38:02 +0700

    Update

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 pkg install nodejs -y
 pkg install ffmpeg -y
 pkg install imagemagick -y
+apt install clang libpixman librsvg giflib libpng libjpeg-turbo pango libcairo xorgproto
+npm i
 npm install
 npm update
