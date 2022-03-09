416cd162509d4177bb12ab8eaf8fd24dfb24ff02
unxzx <84166927+unx21@users.noreply.github.com>
2022-03-09 12:13:14 +0800

    Horny sticker

---
+++ plugins/hornycard.js
@@ -0,0 +1,29 @@
+const uploadImage = require('../lib/uploadImage')
+const { sticker } = require('../lib/sticker')
+const { MessageType } = require('@adiwajshing/baileys')
+
+let handler = async (m, { conn, text }) => {
+try {
+  let q = m.quoted ? m.quoted : m
+  let mime = (q.msg || q).mimetype || ''
+  if (!mime) throw 'Tidak ada foto'
+  if (!/image\/(jpe?g|png)/.test(mime)) throw `Mime ${mime} tidak support`
+  await m.reply(wait)
+  let img = await q.download()
+  let url = await uploadImage(img)
+  let Horny = `https://some-random-api.ml/canvas/horny?avatar=${url}`
+  let stiker = await sticker(null, Horny, global.packname, global.author)
+  conn.sendMessage(m.chat, stiker, MessageType.sticker, {
+    quoted: m
+  })
+} catch (e) {
+  m.reply('Conversion Failed')
+  }
+}
+
+
+handler.help = ['hornycard (reply|balas foto)', 'hornylicense (reply|balas foto)']
+handler.tags = ['sticker']
+handler.command = /^horny(card|license)$/i
+
+module.exports = handler
