154e3bc698c4fee8cd76ce67cea16edc14869582
aldinoalamsyah <102845077+aldinoalamsyah@users.noreply.github.com>
2022-04-02 12:08:15 +0700

    Update installing

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 pkg install nodejs -y
 pkg install ffmpeg -y
 pkg install imagemagick -y
+pkg install yarn
 apt install clang libpixman librsvg giflib libpng libjpeg-turbo pango libcairo xorgproto
 npm i
 npm install
 npm update
+yarn
+node . --public
