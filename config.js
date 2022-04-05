2d3bd994a3802ba4d007b81d48edd4c12915509f
Inichika <siskarahmania25@gmail.com>
2022-04-05 17:06:33 +0700

    :/

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 /*
+
 Thanks to
 
 • Nurutomo
@@ -8,7 +9,7 @@ Thanks to
 • MRHRTZ hanif
 • Bochilgaming
 • unx
-• and all creator
+• and all contributor
 
 */
 
@@ -21,11 +22,10 @@ global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Pengguna pre
 
 global.APIs = { // API Prefix
   // nama: 'https://website'
-  chika: 'https://chikaeluvie.eu.org',
-  rey: 'https://api.reysekha.xyz', // CHANGED TO https://reysekhaa.herokuapp.com
+  rey: 'https://sekha.tech', // ERORR? CHANGED TO https://reysekhaa.herokuapp.com
   hardianto: 'https://hardianto.xyz',
   jonaz: 'https://jonaz-api-v2.herokuapp.com',
-  neoxr: 'https://api.neoxr.eu.org',
+  neoxr: 'https://api.neoxr.my.id',
   nrtm: 'https://nurutomo.herokuapp.com',
   pencarikode: 'https://api.chipa.xyz',
   xteam: 'https://api.xteam.xyz',
@@ -33,22 +33,22 @@ global.APIs = { // API Prefix
   waifupics: 'https://api.waifu.pics',
   cakra: 'https://cakrayp.herokuapp.com',
   zahir: 'https://zahirr-web.herokuapp.com',
-  zekais: 'http://zekais-api.herokuapp.com',
+  zekais: 'http://api.zekais.com',
   zeks: 'https://api.zeks.me',
   amel: 'https://melcanz.com',
 }
 global.APIKeys = { // APIKey in here
   // 'https://website': 'apikey'
-  'https://chikaeluvie.eu.org': 'APIKEYMU',
   'https://api-xcoders.xyz': 'APIKEY', // REGISTER ON WEB
-  'https://api.reysekha.xyz': 'apirey', // CHANGED TO https://reysekhaa.herokuapp.com
+  'https://sekha.tech': 'apirey', // ERORR? CHANGED TO https://reysekhaa.herokuapp.com
   'https://hardianto.xyz': 'hardianto', // UNLIMITED
-  'https://api.neoxr.eu.org': 'yourkey', // UNLIMITED
+  'https://api.neoxr.my.id': 'yourkey', // UNLIMITED
   'https://api.chipa.xyz': 'APIKEY', // REGISTER ON WEB
   'https://cakrayp.herokuapp.com': 'cakrayp24Q6', // UNLIMITED
   'https://api.xteam.xyz': 'APIKEY', // REGISTER ON WEB
   'https://zahirr-web.herokuapp.com': 'zahirgans', // UNLIMITED
   'https://api.zeks.me': 'apivinz', // LIMIT 400 day 
+  'http://api.zekais.com',: 'APIKEY', // REGISTER
   'https://melcanz.com': 'YOURAPIKEY' // Register dulu
 }
 
