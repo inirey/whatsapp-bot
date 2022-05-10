71facb3d3f4f4aaf17fd2a554a2df8f77f6d3978
Reysekha <me@sekha.tech>
2022-05-10 21:47:19 +0700

    Delete plugins directory

--- plugins/getcode.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn, usedPrefix }) => {
-  if (global.conn.user.jid == conn.user.jid) conn.reply(m.chat, 'Perintah ini hanya untuk yang jadi bot', m)
-  else global.conn.reply(conn.user.jid, `${usedPrefix}jadibot ${Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString('base64')}`, m)
-}
-handler.help = ['getcode']
-handler.tags = ['jadibot']
-handler.command = /^(getcode)$/i
-
-module.exports = handler
